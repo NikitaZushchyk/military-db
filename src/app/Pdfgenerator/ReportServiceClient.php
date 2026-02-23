6ea3984dada65b6f770cf62e156b1a3a11ba8e36
NikitaZushchyk <250507475+NikitaZushchyk@users.noreply.github.com>
2026-02-23 17:29:36 +0000

    style: auto-fix code style (PSR-12)

--- src/app/Pdfgenerator/ReportServiceClient.php
+++ src/app/Pdfgenerator/ReportServiceClient.php
@@ -1,32 +1,32 @@
 <?php
+
 // GENERATED CODE -- DO NOT EDIT!
 
 namespace Pdfgenerator;
 
-/**
- */
-class ReportServiceClient extends \Grpc\BaseStub {
-
+class ReportServiceClient extends \Grpc\BaseStub
+{
     /**
-     * @param string $hostname hostname
-     * @param array $opts channel options
-     * @param \Grpc\Channel $channel (optional) re-use channel object
+     * @param  string  $hostname  hostname
+     * @param  array  $opts  channel options
+     * @param  \Grpc\Channel  $channel  (optional) re-use channel object
      */
-    public function __construct($hostname, $opts, $channel = null) {
+    public function __construct($hostname, $opts, $channel = null)
+    {
         parent::__construct($hostname, $opts, $channel);
     }
 
     /**
-     * @param \Pdfgenerator\UniversalTableRequest $argument input argument
-     * @param array $metadata metadata
-     * @param array $options call options
+     * @param  \Pdfgenerator\UniversalTableRequest  $argument  input argument
+     * @param  array  $metadata  metadata
+     * @param  array  $options  call options
      */
     public function GenerateTablePdf(\Pdfgenerator\UniversalTableRequest $argument,
-      $metadata = [], $options = []) {
+        $metadata = [], $options = [])
+    {
         return $this->_simpleRequest('/pdfgenerator.ReportService/GenerateTablePdf',
-        $argument,
-        ['\Pdfgenerator\PdfResponse', 'decode'],
-        $metadata, $options);
+            $argument,
+            ['\Pdfgenerator\PdfResponse', 'decode'],
+            $metadata, $options);
     }
-
 }
