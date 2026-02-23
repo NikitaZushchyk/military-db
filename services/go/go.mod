f942c0d52c45e097bfc445af26a180cba13c0ed2
Nikita Zushchyk <n.zuschyk@adducates.com>
2026-02-23 12:57:53 +0000

    Add go service logic

--- services/go/go.mod
+++ services/go/go.mod
@@ -1,3 +1,20 @@
 module go-service
 
-go 1.22
+go 1.24.0
+
+require (
+	github.com/johnfercher/maroto v1.0.0
+	google.golang.org/grpc v1.79.1
+	google.golang.org/protobuf v1.36.11
+)
+
+require (
+	github.com/boombuler/barcode v1.0.1 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/jung-kurt/gofpdf v1.16.2 // indirect
+	github.com/ruudk/golang-pdf417 v0.0.0-20201230142125-a7e3863a1245 // indirect
+	golang.org/x/net v0.48.0 // indirect
+	golang.org/x/sys v0.39.0 // indirect
+	golang.org/x/text v0.32.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20251202230838-ff82c1b0f217 // indirect
+)
--- services/go/main.go
+++ services/go/main.go
@@ -1,21 +1,84 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"github.com/johnfercher/maroto/pkg/consts"
+	"github.com/johnfercher/maroto/pkg/pdf"
+	"github.com/johnfercher/maroto/pkg/props"
+	"go-service/pb"
+	"google.golang.org/grpc"
 	"log"
 	"net"
 )
 
-func main() {
-	fmt.Println("🚀 Go мікросервіс запускається...")
+type PdfServer struct {
+	pb.UnimplementedReportServiceServer
+}
 
-	port := ":50051"
-	_, err := net.Listen("tcp", port)
-	if err != nil {
-		log.Fatalf("❌ Не вдалося запустити слухача на порту %s: %v", port, err)
+func (s *PdfServer) GenerateTablePdf(ctx context.Context, req *pb.UniversalTableRequest) (*pb.PdfResponse, error) {
+	fmt.Printf("[INFO] Received request to generate report: %s\n", req.Title)
+
+	m := pdf.NewMaroto(consts.Portrait, consts.A4)
+
+	m.AddUTF8Font("Roboto", consts.Normal, "fonts/Roboto-Regular.ttf")
+	m.AddUTF8Font("Roboto", consts.Bold, "fonts/Roboto-Bold.ttf")
+	m.SetDefaultFontFamily("Roboto")
+
+	m.RegisterHeader(func() {
+		m.Row(20, func() {
+			m.Col(12, func() {
+				m.Text(req.Title, props.Text{
+					Top:   5,
+					Style: consts.Bold,
+					Align: consts.Center,
+					Size:  16,
+				})
+			})
+		})
+	})
+
+	// Підготовка даних
+	var tableData [][]string
+	for _, row := range req.Rows {
+		tableData = append(tableData, row.Cells)
 	}
 
-	fmt.Printf("✅ Сервер успішно слухає порт %s. Очікування запитів...\n", port)
+	// Малюємо таблицю
+	m.TableList(req.Headers, tableData, props.TableList{
+		HeaderProp: props.TableListContent{
+			Style: consts.Bold,
+		},
+		Align: consts.Left,
+	})
 
-	select {}
+	buf, err := m.Output()
+	if err != nil {
+		fmt.Printf("[ERROR] Failed to generate PDF: %v\n", err)
+		return nil, err
+	}
+
+	response := &pb.PdfResponse{
+		FileData: buf.Bytes(),
+	}
+
+	fmt.Println("[SUCCESS] PDF successfully generated")
+	return response, nil
+}
+
+func main() {
+	port := ":50051"
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("[FATAL] Failed to listen on port %s: %v", port, err)
+	}
+
+	grpcServer := grpc.NewServer()
+	pb.RegisterReportServiceServer(grpcServer, &PdfServer{})
+
+	fmt.Printf("[INFO] gRPC Server is running and listening on port %s...\n", port)
+
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("[FATAL] Failed to serve gRPC server: %v", err)
+	}
 }
